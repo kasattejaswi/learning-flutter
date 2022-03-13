6609e58801d67df0a291ecda20a71c9b47fedcad
Tejaswi Kasat <kasattejasvi@gmail.com>
2022-03-13 18:26:27 +0530

    Moved home to separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,14 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Tejaswi";
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          )
-        ),
-      )
+      home: HomePage()
     );
   }
 }
