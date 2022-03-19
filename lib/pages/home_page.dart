7cec7d52066ed19c4e9d4dbe7f73125ecb8e8497
Tejaswi Kasat <kasattejasvi@gmail.com>
2022-03-19 20:02:38 +0530

    Added list view at main page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/models/catalog.dart';
 import 'package:untitled/widgets/drawer.dart';
+import 'package:untitled/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final int days = 30;
     final String name = "Tejaswi";
+    final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalogue app"),
       ),
-      body: Center(
-          child: Container(
-        child: Text("Welcome to $days days of flutter by $name."),
-      )),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
+      ),
       drawer: MyDrawer(),
     );
   }
